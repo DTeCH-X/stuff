a5ab6e896fc4bb1963510051d0e10bc87da45afd
DTeCH-X <dtechzone@gmail.com>
2015-05-29 23:05:44 -0500

    Anti-GoogleSpyLogging
    Anti-GoogleSpyLogging

---
+++ Anti-GoogleSpyLogging.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Anti-GoogleSpyLogging - (DTeCH)
+// @namespace    Anti-GoogleSpyLogging
+// @version      0.9.92
+// @description  Stops Google search engine logging of your website visits clicked from it's search result pages, but does NOT help against the initial searched tearms that generated the results. Google is rumored to be NSA run, & funded.
+// @author       DTeCH
+// @icon         http://www.tunisie-news.com/high-tech/images/Google+FBI.jpg
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js
+// @include      http*://www.google.com/*
+// @updateURL    https://openuserjs.org/src/libs/DTeCH/AGSL.meta.js
+// @downloadURL  https://openuserjs.org/install/DTeCH/Anti-GoogleSpyLogging_-_(DTeCH).user.js
+// @grant        none
+// ==/UserScript==
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(1(){$(\'2[0="6"][0="7"][0="8"][0="9"][0="b"]\').3(1(){$(4).5()});$(\'2[0="6"]\').3(1(){$(4).5()});$(\'2[0="7"]\').3(1(){$(4).5()});$(\'2[0="8"]\').3(1(){$(4).5()});$(\'2[0="9"]\').3(1(){$(4).5()});$(\'2[0="b"]\').3(1(){$(4).5()});$("a").c("d")})();',14,14,'name|function|input|each|this|remove|oq|gs_l|btnI|btnK||es_sm|removeAttr|onmousedown'.split('|'),0,{}))
