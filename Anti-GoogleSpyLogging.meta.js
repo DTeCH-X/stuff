2adec5dd8feaeb8d8d5880b0e99983c795cdfd19
DTeCH-X <dtechzone@gmail.com>
2015-05-29 23:17:55 -0500

    Create Anti-GoogleSpyLogging.meta.js
    Anti-GoogleSpyLogging.meta.js

---
+++ Anti-GoogleSpyLogging.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Anti-GoogleSpyLogging - (DTeCH)
+// @namespace    Anti-GoogleSpyLogging
+// @version      0.9.92
+// @description  Stops Google search engine logging of your website visits clicked from it's search result pages, but does NOT help against the initial searched tearms that generated the results. Google is rumored to be NSA run, & funded.
+// @author       DTeCH
+// @icon         http://www.tunisie-news.com/high-tech/images/Google+FBI.jpg
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js
+// @include      http*://www.google.com/*
+// @updateURL    https://openuserjs.org/src/libs/DTeCH/AGSL.meta.js
+// @downloadURL  https://github.com/DTeCH-X/stuff/raw/master/Anti-GoogleSpyLogging.user.js
+// @grant        none
+// ==/UserScript==
