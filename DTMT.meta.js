1111186b2968621053a82689a03a1cf2a8775edd
DTeCH-X <dtechzone@gmail.com>
2015-06-01 18:23:05 -0400

    update commit

---
+++ DTMT.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        MaelstromTools Dev (DTeCH)
+// @namespace   DTMaelstromTools
+// @icon        http://eistee82.github.io/ta_simv2/icon.png
+// @require     https://cdn.rawgit.com/DTeCH-X/stuff/master/DTTAW.user.js
+// @description Just a set of statistics & summaries about repair time and base resources. Mainly for internal use, but you are free to test and comment it.
+// @version     2.1.1i
+// @author      DTeCH, Maelstrom, HuffyLuf, KRS_L, && Krisan
+// @include     http*://prodgame*.alliances.commandandconquer.com/*/index.aspx*
+// @updateURL   https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMT.meta.js
+// @downloadURL https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMT.user.js
+// ==/UserScript==
+
---
+++ DTTAT.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name           Tiberium Alliances Tweaks - DTeCH
+// @version        2.1.1i
+// @namespace      DTTAT
+// @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @author         DTeCH, petui
+// @description    A collection of more or less useful features that attempt to improve the gaming experience.
+// @include        http*://prodgame*.alliances.commandandconquer.com/*/index.aspx*
+// ==/UserScript==
