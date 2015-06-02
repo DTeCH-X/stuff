a968bdfb6bbab9ca597fe5ec09f7afe95006d57f
DTeCH-X <dtechzone@gmail.com>
2015-06-02 03:12:26 -0500

    Create DTTAZ.meta.js

---
+++ DTTAZ.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name           TA Zoom - DTeCH
+// @description    Increased Tiberium Alliances Zoom
+// @namespace      TAZoomDTeCH
+// @include        https://prodgame*.alliances.commandandconquer.com/*/index.aspx*
+// @version        2.1.1i
+// @author         DTeCH, Panavia
+// @icon           http://eistee82.github.io/ta_simv2/icon.png
+// @updateURL      https://cdn.rawgit.com/DTeCH-X/stuff/master/DTTAZ.meta.js
+// @downloadURL    https://cdn.rawgit.com/DTeCH-X/stuff/master/DTTAZ.user.js
+// ==/UserScript==
