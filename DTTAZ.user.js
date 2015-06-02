1ecd48108d455c48e8645483a4ade3ac00aa90b5
DTeCH-X <dtechzone@gmail.com>
2015-06-02 03:11:22 -0500

    Create DTTAZ.user.js

---
+++ DTTAZ.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           TA Zoom - DTeCH
+// @description    Increased Tiberium Alliances Zoom
+// @namespace      TAZoomDTeCH
+// @include        https://prodgame*.alliances.commandandconquer.com/*/index.aspx*
+// @version        2.1.1i
+// @author         DTeCH, Panavia
+// @icon           http://eistee82.github.io/ta_simv2/icon.png
+// @updateURL      https://cdn.rawgit.com/DTeCH-X/stuff/master/DTTAZ.meta.js
+// @downloadURL    https://cdn.rawgit.com/DTeCH-X/stuff/master/DTTAZ.user.js
+// ==/UserScript==
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('T{(8(){3 H=8(){8 I(){r.E("1q 1p");3 g=1;3 o=0.2;3 b=0.15;U.V.X.11.1n=8(D){5(!7.1m||D.1j()!=7.C){Q}3 R=D.1i();3 f=1h.1g.1f.1e();1c(R){10"+":3 s=f.p().z()+b;f.p().y(9.A(g,9.v(o,s)));M;10"-":3 s=f.p().z()-b;f.p().y(9.A(g,9.v(o,s)));M;1b:}7.1a();7.19()};3 4=6.w.x.G().18();6.l.k.W(4.C,"h",4.c,4);6.l.k.W(4.Z,"h",4.c,4);U.V.X.11.c=8(e){5(7.F==17){Q}3 13=e.16();3 d=7.F.z();d+=13>0?-b:b;d=9.A(g,9.v(o,d));7.F.y(d);e.1o()};6.l.k.12(4.C,"h",4.c,4);6.l.k.12(4.Z,"h",4.c,4)}8 q(){T{5(O 6!="N"){3 a=6.w.x.G();3 Y=6.w.x.G().1d();5(a&&Y){I()}n{m.u(q,B)}}n{m.u(q,B)}}S(e){5(O r!="N"){r.E(e)}n 5(m.L){L.1k(e)}n{1l(e)}}}5(/K\\.14/i.P(j.J)){m.u(q,B)}};3 t=j.1r("1s");t.1t="("+H.1u()+")();";t.1v="1w/1x";5(/K\\.14/i.P(j.J)){j.1y("1z")[0].1A(t)}})()}S(e){r.E("1B 1C 1D-1E 1F: ",e)}',62,104,'|||var|backgroundArea|if|qx|this|function|Math||zoomInc|_onMouseWheel|by||bf|zoomMin|mousewheel||document|Element|bom|window|else|zoomMax|get_Region|tazoom_checkIfLoaded|console|bg|tazoomScript|setTimeout|max|core|Init|set_ZoomFactor|get_ZoomFactor|min|1000|mapContainer|be|log|activeSceneView|getApplication|tazoom_main|initialize|domain|commandandconquer|opera|break|undefined|typeof|test|return|bh|catch|try|webfrontend|gui|removeListener|BackgroundArea|mb|mapBlocker|case|prototype|addListener|bz|com|05|getWheelDelta|null|getBackgroundArea|closeCityList|closeCityInfo|default|switch|getMenuBar|GetInstance|VisMain|Vis|ClientLib|getKeyIdentifier|getTarget|postError|GM_log|active|onHotKeyPress|stop|Loaded|Zoom|createElement|script|innerHTML|toString|type|text|javascript|getElementsByTagName|head|appendChild|TA|Script|Bug|Fixes|Pack'.split('|'),0,{}))
