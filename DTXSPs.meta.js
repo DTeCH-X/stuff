2a168102c96533c2f3d2711320c071ff19e7aaef
DTeCH-X <dtechzone@gmail.com>
2015-07-27 11:09:34 -0400

    Update DTXSPs.meta.js

--- DTXSPs.meta.js
+++ DTXSPs.meta.js
@@ -1,1 +1,23 @@
 
+// ==UserScript==
+// @name          TA Script Bug-Fixes Pack - (DTeCH)
+// @namespace     TAScriptBugFixesPack
+// @icon          http://eistee82.github.io/ta_simv2/icon.png
+// @description   Maelstrom, Basescanner, CNCOpt (Offence), Coords, Combat Sim, ZWaves, Who's Online, Tunnel Info, Resource Trade Window, ReplayShare, TA Fixes, TA Base Upgrade Tool, & Zoom
+// @include       http*://prodgame*.alliances.commandandconquer.com/*/index.aspx*
+// @include       http*://*.cncopt.com/*
+// @include       http*://cncopt.com/*
+// @grant         GM_log
+// @grant         GM_setValue
+// @grant         GM_getValue
+// @grant         GM_openInTab
+// @grant         GM_registerMenuCommand
+// @grant         GM_xmlhttpRequest
+// @grant         GM_updatingEnabled
+// @grant         unsafeWindow
+// @grant         metadata
+// @version       2.1.5gs
+// @author        DTeCH
+// @updateURL     https://cdn.rawgit.com/DTeCH-X/stuff/master/DTXSPs.meta.js
+// @downloadURL   https://cdn.rawgit.com/DTeCH-X/stuff/master/DTXSPs215g.user.js
+// ==/UserScript==
