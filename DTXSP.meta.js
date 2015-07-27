05614620da648c6c56f2bec03a94050662eead9d
DTeCH-X <dtechzone@gmail.com>
2015-07-27 09:51:54 -0400

    Update DTXSP.meta.js

--- DTXSP.meta.js
+++ DTXSP.meta.js
@@ -15,8 +15,8 @@
 // @grant         GM_updatingEnabled
 // @grant         unsafeWindow
 // @grant         metadata
-// @version       2.1.4a
+// @version       2.1.5g
 // @author        DTeCH
 // @updateURL     https://cdn.rawgit.com/DTeCH-X/stuff/master/DTXSP.meta.js
-// @downloadURL   https://cdn.rawgit.com/DTeCH-X/stuff/master/DTXSP214a.user.js
+// @downloadURL   https://cdn.rawgit.com/DTeCH-X/stuff/master/DTXSP215g.user.js
 // ==/UserScript==
