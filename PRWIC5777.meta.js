5088971a2d98969d70b9532c258e9623e4fd5360
DTeCH-X <dtechzone@gmail.com>
2015-12-30 21:57:32 -0500

    Update PRWIC5777.meta.js

--- PRWIC5777.meta.js
+++ PRWIC5777.meta.js
@@ -4,10 +4,10 @@
 // @version      5.7.7
 // @description  primewire.ag (PrWi-C) Ad Cleaner removes annoying ads from the Primewire.ag free movie site. Some forcefull redirect ads require AdBlock Plus or something similar to get rid of if required.
 // @author       DTeCH
-// @icon         http://tagmarks.org/thumbsets/default/PrimeWireAg.png
+// @icon         http://www.listsforall.com/wp-content/uploads/2015/10/PrimeWireAg.png
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js
 // @include      http*://*.primewire.ag/*
-// @updateURL    https://openuserjs.org/src/libs/DTeCH/PRWIC5777.meta.js
-// @downloadURL  https://openuserjs.org/install/DTeCH/PRWIC577.user.js
+// @updateURL    https://cdn.rawgit.com/DTeCH-X/stuff/master/PRWIC5771.meta.js
+// @downloadURL  https://cdn.rawgit.com/DTeCH-X/stuff/master/PRWIC577.user.js
 // @grant        GM_addStyle
 // ==/UserScript== 
