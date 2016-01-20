eaaa28c55f6dec3790562b3fb8372645c6583416
DTeCH-X <dtechzone@gmail.com>
2016-01-20 11:14:45 -0500

    Create DTXSP301.meta.js

---
+++ DTXSP301.meta.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name          TA Script Bug-Fixes Pack - (DTeCH)
+// @namespace     TAScriptBugFixesPack
+// @icon          http://eistee82.github.io/ta_simv2/icon.png
+// @description   MaelstromTools Dev, Basescanner, X-Ray, Coords, Combat Sim, ZWaves, Who's Online, Tunnel Info, Chat Colorizer, ReplayShare, TA Fixes, Resource Trade Window, & TA Base Upgrade Tool
+// @include       http*://prodgame*.alliances.commandandconquer.com/*/index.aspx*
+// @include       http*://*.cncopt.com/*
+// @include       http*://cncopt.com/*
+// @grant         GM_log
+// @grant         GM_setValue
+// @grant         GM_getValue
+// @grant         GM_openInTab
+// @grant         GM_registerMenuCommand
+// @grant         GM_xmlhttpRequest
+// @grant         GM_updatingEnabled
+// @grant         unsafeWindow
+// @grant         metadata
+// @version       3.0.1
+// @author        DTeCH
+// @updateURL     https://cdn.rawgit.com/DTeCH-X/stuff/master/DTXSP301.meta.js
+// @downloadURL   https://cdn.rawgit.com/DTeCH-X/stuff/master/DTXSP301.user.js
+// ==/UserScript==
