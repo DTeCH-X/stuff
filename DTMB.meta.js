2348abe926ec1eff97796b156325edd50be9b74d
DTeCH-X <dtechzone@gmail.com>
2016-09-14 03:29:09 -0500

    Create DTMB.meta.js
    MB Game Meta File

---
+++ DTMB.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         MafiaBattle Auto-Collector
+// @namespace    https://www.mafiabattle.com
+// @icon         https://www.mafiabattle.com/images/favicon/default.png
+// @version      1.9.8
+// @description  Try to take over the world in MafiaBattle!
+// @author       DTeCH
+// @match        https://www.mafiabattle.com/partner/iframe/*
+// @updateURL    https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB.meta.js
+// @downloadURL  https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB.user.js
+// @grant        none
+// ==/UserScript==
