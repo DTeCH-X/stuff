0b0949a68a1e6bb5d8bfd4ab272ee160576843f4
DTeCH-X <dtechzone@gmail.com>
2017-07-09 22:54:19 -0500

    Update DTMB265.meta.js

--- DTMB265.meta.js
+++ DTMB265.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         MafiaBattle Auto-Collector v2.6.5
+// @name         MafiaBattle Auto-Collector v2.6.6
 // @namespace    https://www.mafiabattle.com
 // @icon         https://www.mafiabattle.com/images/favicon/default.png
-// @version      2.6.5
+// @version      2.6.6
 // @description  Try to take over the world in MafiaBattle!
 // @author       DTeCH
 // @match        https://www.mafiabattle.com/partner/iframe/*
-// @updateURL    https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB265.meta.js
-// @downloadURL  https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB265.user.js
+// @updateURL    https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB266.meta.js
+// @downloadURL  https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB266.user.js
 // @grant        GM_addStyle
 // ==/UserScript==
