b6918a364ddc1341dffd98defebc3f404e1146bd
DTeCH-X <dtechzone@gmail.com>
2017-07-10 10:05:23 -0500

    Update and rename DTMB267.meta.js to DTMB268.meta.js

--- DTMB268.meta.js
+++ DTMB268.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         MafiaBattle Auto-Collector v2.6.7
+// @name         MafiaBattle Auto-Collector v2.6.8
 // @namespace    https://www.mafiabattle.com
 // @icon         https://www.mafiabattle.com/images/favicon/default.png
-// @version      2.6.7
+// @version      2.6.8
 // @description  Try to take over the world in MafiaBattle!
 // @author       DTeCH
 // @match        https://www.mafiabattle.com/partner/iframe/*
-// @updateURL    https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB267.meta.js
-// @downloadURL  https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB267.user.js
+// @updateURL    https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB268.meta.js
+// @downloadURL  https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB268.user.js
 // @grant        GM_addStyle
 // ==/UserScript==
