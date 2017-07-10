7bf0778424d90a3172e203a7f56996a0eac90100
DTeCH-X <dtechzone@gmail.com>
2017-07-09 21:03:55 -0500

    Update DTMB263.meta.js

--- DTMB263.meta.js
+++ DTMB263.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         MafiaBattle Auto-Collector v2.6.4
+// @name         MafiaBattle Auto-Collector v2.6.5
 // @namespace    https://www.mafiabattle.com
 // @icon         https://www.mafiabattle.com/images/favicon/default.png
-// @version      2.6.4
+// @version      2.6.5
 // @description  Try to take over the world in MafiaBattle!
 // @author       DTeCH
 // @match        https://www.mafiabattle.com/partner/iframe/*
