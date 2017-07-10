ec6d96711c9b698facd7ba234020dbb9d8d0e3a0
DTeCH-X <dtechzone@gmail.com>
2017-07-09 20:53:52 -0500

    Create DTMB263.meta.js

---
+++ DTMB263.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         MafiaBattle Auto-Collector v2.6.4
+// @namespace    https://www.mafiabattle.com
+// @icon         https://www.mafiabattle.com/images/favicon/default.png
+// @version      2.6.4
+// @description  Try to take over the world in MafiaBattle!
+// @author       DTeCH
+// @match        https://www.mafiabattle.com/partner/iframe/*
+// @updateURL    https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB263.meta.js
+// @downloadURL  https://cdn.rawgit.com/DTeCH-X/stuff/master/DTMB263.user.js
+// @grant        GM_addStyle
+// ==/UserScript==
